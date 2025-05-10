be07c495dfb0c362020804fe7c555092e699cd1a
Huynh Thanh Tan <tan.huynh@anfin.vn>
2025-05-11 01:03:07 +0700

    chore: deploy models

--- app.py
+++ app.py
@@ -62,8 +62,8 @@ def upload_and_extract_table():
     # Sidebar Configuration UI
     st.sidebar.title("Configuration")
     structure_extraction_method = st.sidebar.radio(
-        "Choose method",
-        options=["Contours Detection (Traditional, heuristic method)", "UniTable (Latest, unified, SOTA method)", "Table Transformer (A deep-learning with CNN and Transformer, also called a DERT method)"],
+        "Choose extraction method",
+        options=["Contours Detection", "UniTable", "Table Transformer"],
         help="Select how you want to extract tables from the uploaded image.",
     )
 
@@ -100,7 +100,9 @@ def upload_and_extract_table():
         st.warning("Displaying images is supported only for PNG, JPG, and JPEG formats.")
 
     if st.sidebar.button('Run'):
-        st.write("You pressed the Run button!")
+        st.sidebar.info("The engine is running to serve you the best result...")
+
+        st.info("The result will be displayed here")
         if structure_extraction_method == "Contours Detection":
             table_detected_image, contours = detect_and_show_table(file)
             st.image(table_detected_image, caption="Detected Table(s)", use_container_width=True)
@@ -115,8 +117,157 @@ def upload_and_extract_table():
                     st.download_button("Download as CSV", table.to_csv(index=False), "table.csv")
                 else:
                     st.warning("No text detected in the table region.")
+        elif structure_extraction_method == "UniTable":
+            st.sidebar.title("Elapsed Time")
+            from PIL import Image
+            img = Image.open(file)
+            # st.info(f'Data={process_image(img, False, "unitable", True, True, 15, 10)}')
+            img_input = img
+            small_box_cut_enhance = True
+            table_engine_type = "unitable"  # Example, replace with actual method
+            char_ocr = True
+            rotated_fix = True
+            col_threshold = 15
+            row_threshold = 10
+
+            complete_html, table_boxes_img, ocr_boxes_img, all_elapse = process_image(
+                img_input,
+                small_box_cut_enhance,
+                table_engine_type,
+                char_ocr,
+                rotated_fix,
+                col_threshold,
+                row_threshold,
+            )
+
+            st.title("Html Render")
+            st.markdown(complete_html, unsafe_allow_html=True)  # Rendering the HTML content
+
+            st.title("Table Boxes")
+            st.image(table_boxes_img, caption="Detected Table Boxes", use_column_width=True)
+
+            st.title("OCR Boxes")
+            st.image(ocr_boxes_img, caption="Detected OCR Boxes", use_column_width=True)
+
+            st.sidebar.write(all_elapse)
+        elif structure_extraction_method == "Table Transformer":
+            st.sidebar.warning("Table Transformer is not implemented yet.")
         else:
-            st.warning("No tables detected in the uploaded image.")
+            st.sidebar.warning("No engine selected.")
+        st.sidebar.info("The engine has finished the task. Please check the result below.")
+
+
+from utils.unitable_util import plot_rec_box, LoadImage, format_html, box_4_2_poly_to_box_4_1
+import time
+from lineless_table_rec import LinelessTableRecognition
+from rapid_table import RapidTable, RapidTableInput
+from wired_table_rec import WiredTableRecognition
+from rapid_table.main import ModelType
+from table_cls import TableCls
+from rapidocr_onnxruntime import RapidOCR
+img_loader = LoadImage()
+
+from wired_table_rec.main import WiredTableInput, WiredTableRecognition
+from lineless_table_rec.main import LinelessTableInput, LinelessTableRecognition
+
+rapid_table_engine = RapidTable(RapidTableInput(model_type=ModelType.PPSTRUCTURE_ZH.value, model_path="models/tsr/ch_ppstructure_mobile_v2_SLANet.onnx"))
+SLANet_plus_table_Engine = RapidTable(RapidTableInput(model_type=ModelType.SLANETPLUS.value, model_path="models/tsr/slanet-plus.onnx"))
+unitable_table_Engine = RapidTable(RapidTableInput(model_type=ModelType.UNITABLE.value, model_path={
+            "encoder": f"models/tsr/unitable_encoder.pth",
+            "decoder": f"models/tsr/unitable_decoder.pth",
+            "vocab": f"models/tsr/unitable_vocab.json",
+        }))
+
+wired_input = WiredTableInput()
+lineless_input = LinelessTableInput()
+wired_engine = WiredTableRecognition(wired_input)
+wired_table_engine_v1 = wired_engine
+wired_table_engine_v2 = wired_engine
+lineless_engine = LinelessTableRecognition(lineless_input)
+det_model_dir = {
+    "mobile_det": "models/ocr/ch_PP-OCRv4_det_infer.onnx",
+}
+
+rec_model_dir = {
+    "mobile_rec": "models/ocr/ch_PP-OCRv4_rec_infer.onnx",
+}
+ocr_engine_dict = {}
+for det_model in det_model_dir.keys():
+    for rec_model in rec_model_dir.keys():
+        det_model_path = det_model_dir[det_model]
+        rec_model_path = rec_model_dir[rec_model]
+        key = f"{det_model}_{rec_model}"
+        ocr_engine_dict[key] = RapidOCR(det_model_path=det_model_path, rec_model_path=rec_model_path)
+
+def select_table_model(img, table_engine_type, det_model, rec_model):
+    return  unitable_table_Engine, table_engine_type
+    # if table_engine_type == "RapidTable(SLANet)":
+    #     return rapid_table_engine, table_engine_type
+    # elif table_engine_type == "RapidTable(SLANet-plus)":
+    #     return SLANet_plus_table_Engine, table_engine_type
+    # elif table_engine_type == "RapidTable(unitable)":
+    #     return unitable_table_Engine, table_engine_type
+    # elif table_engine_type == "wired_table_v1":
+    #     return wired_table_engine_v1, table_engine_type
+    # elif table_engine_type == "wired_table_v2":
+    #     print("使用v2 wired table")
+    #     return wired_table_engine_v2, table_engine_type
+    # elif table_engine_type == "lineless_table":
+    #     return lineless_table_engine, table_engine_type
+    # elif table_engine_type == "auto":
+    #     cls, elasp = table_cls(img)
+    #     if cls == 'wired':
+    #         table_engine = wired_table_engine_v2
+    #         return table_engine, "wired_table_v2"
+    #     return lineless_table_engine, "lineless_table"
+
+def select_ocr_model(det_model, rec_model):
+    return ocr_engine_dict[f"{det_model}_{rec_model}"]
+
+def trans_char_ocr_res(ocr_res):
+    word_result = []
+    for res in ocr_res:
+        score = res[2]
+        for word_box, word in zip(res[3], res[4]):
+            word_res = []
+            word_res.append(word_box)
+            word_res.append(word)
+            word_res.append(score)
+            word_result.append(word_res)
+    return word_result
+
+def process_image(img_input, small_box_cut_enhance, table_engine_type, char_ocr, rotated_fix, col_threshold, row_threshold):
+    det_model="mobile_det"
+    rec_model="mobile_rec"
+    img = img_loader(img_input)
+    start = time.time()
+    table_engine, table_type = select_table_model(img, table_engine_type, det_model, rec_model)
+    ocr_engine = select_ocr_model(det_model, rec_model)
+
+    ocr_res, ocr_infer_elapse = ocr_engine(img, return_word_box=char_ocr)
+    det_cost, cls_cost, rec_cost = ocr_infer_elapse
+    if char_ocr:
+        ocr_res = trans_char_ocr_res(ocr_res)
+    ocr_boxes = [box_4_2_poly_to_box_4_1(ori_ocr[0]) for ori_ocr in ocr_res]
+    if isinstance(table_engine, RapidTable):
+        table_results = table_engine(img, ocr_res)
+        html, polygons, table_rec_elapse = table_results.pred_html, table_results.cell_bboxes,table_results.elapse
+        polygons = [[polygon[0], polygon[1], polygon[4], polygon[5]] for polygon in polygons]
+    elif isinstance(table_engine, (WiredTableRecognition, LinelessTableRecognition)):
+        html, table_rec_elapse, polygons, logic_points, ocr_res = table_engine(img, ocr_result=ocr_res,
+                                                                                   enhance_box_line=small_box_cut_enhance,
+                                                                                   rotated_fix=rotated_fix,
+                                                                                   col_threshold=col_threshold,
+                                                                                   row_threshold=row_threshold)
+    sum_elapse = time.time() - start
+    all_elapse = f"- Table Type: {table_type}\n - Table all cost: {sum_elapse:.5f} seconds\n - Table rec cost: {table_rec_elapse:.5f} seconds\n - OCR cost: {det_cost + cls_cost + rec_cost:.5f} seconds"
+
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    table_boxes_img = plot_rec_box(img.copy(), polygons)
+    ocr_boxes_img = plot_rec_box(img.copy(), ocr_boxes)
+    complete_html = format_html(html)
+
+    return complete_html, table_boxes_img, ocr_boxes_img, all_elapse
 
 if st.session_state.page == "home":
     st.title("Welcome to Table Snap")
